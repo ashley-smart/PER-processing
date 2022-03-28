cb1744a0a0ee2b3406e8ca13601d56b45f037642
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-03-28 13:42:32 -0700

    Update read_rois.py

--- read_rois.py
+++ read_rois.py
@@ -24,10 +24,10 @@ from read_roi import read_roi_zip
 
 
 #### change this ####
-print('got this far')
-date = '1-7-22'  #name of folder the files are in 
+#print('got this far')
+date = '1-3-22'  #name of folder the files are in 
 video_number = 1   # video number using
-roi_number = 2 #make sure rois are saved with this filename format "RoiSet_vid1"
+roi_number = 1 #make sure rois are saved with this filename format "RoiSet_vid1"
 path_to_date = '/oak/stanford/groups/trc/data/Niyathi/' ##end with / and just before date folder (shouldn't need to change)
    
 ###   ###
@@ -42,7 +42,7 @@ jpeg_file_names = os.listdir(jpeg_path)
 save_path = str(path_to_date) + str(date) + "/analysis/" 
 
 #name of file when completed (keep this the same)
-save_file_name = "Results_video_" + str(video_number) + "NONSENSETEST_python.csv"
+save_file_name = "Results_video_" + str(video_number) + "_python.csv"
 
 if os.path.exists(save_path):
     print('save path folder there')
