e6f2e03fd12131e1f07fc5a5f4140aecf8558210
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-06-02 17:44:24 -0700

    Update run_rois.sh

--- run_rois.sh
+++ run_rois.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=4-2read_rois
+#SBATCH --job-name=1214read_rois
 #SBATCH --time=4-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=trc
 #SBATCH --cpus-per-task=2
-#SBATCH --output=./roi-logs/mainlog.out
+#SBATCH --output=./roi-logs/mainlog2.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
 
