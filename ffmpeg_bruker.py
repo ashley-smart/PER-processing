f6fcb79e1f396df2593571e335b7d507fbf010c9
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-06-08 14:59:01 -0700

    Update ffmpeg_bruker.py

--- ffmpeg_bruker.py
+++ ffmpeg_bruker.py
@@ -7,44 +7,48 @@ import subprocess
 import sys
 
 #variables to change
-date = '20210802'    #date folder
+#date = '20210716'    #date folder
+dates = ['20210716', '20210714', '20210709', '20210702', '20210607']
+         
 
-#main path
-video_path = '/oak/stanford/groups/trc/data/Ashley2/bruker videos/' + str(date) +'/'   #should end in /
 
 
 
 def main():
-  #find fly folders
-  for fly_dir in os.listdir(video_path):
-    if 'fly' in fly_dir:
-        fly_video_path = os.path.join(video_path, fly_dir)
-        video_name = os.listdir(fly_video_path)
-        print(fly_video_path)
-        print(video_name)
-        if len(video_name) == 0 or len(video_name) > 1:
-            raise Exception("ERROR: THERE ARE TOO MANY ITEMS IN FOLDER or TOO FEW", video_name)
-            break
-        if '.avi' not in video_name[0]:
-            raise Exception ("ERROR: this file is not avi", video_name[0])
-            break
-        fly_video = os.path.join(fly_video_path, str(video_name[0]))
+  #main path
+  for date in dates: 
+    video_path = '/oak/stanford/groups/trc/data/Ashley2/bruker videos/' + str(date) +'/'   #should end in /
+    
+    #find fly folders
+    for fly_dir in os.listdir(video_path):
+      if 'fly' in fly_dir:
+          fly_video_path = os.path.join(video_path, fly_dir)
+          video_name = os.listdir(fly_video_path)
+          print(fly_video_path)
+          print(video_name)
+          if len(video_name) == 0 or len(video_name) > 1:
+              raise Exception("ERROR: THERE ARE TOO MANY ITEMS IN FOLDER or TOO FEW", video_name)
+              break
+          if '.avi' not in video_name[0]:
+              raise Exception ("ERROR: this file is not avi", video_name[0])
+              break
+          fly_video = os.path.join(fly_video_path, str(video_name[0]))
 
-        # make jpeg path
-        jpeg_path = os.path.join(video_path, 'analysis/', fly_dir)
-        make_dirs(jpeg_path)
-        print(jpeg_path)
-        
-        ## run ffmpeg
-        
-        final_jpeg_path_v1 = os.path.join(jpeg_path, 'V01frame_%07d.jpg')
-        cmd = f'ffmpeg -i REPLACEPATH REPLACEPATHJPEG -hide_banner -loglevel error'  #have to do it this way because I have spaces in the filepath
-        split_version = cmd.split(" ")
-        split_version[2] = fly_video #replaces REPLACEPATH
-        split_version[3] = final_jpeg_path_v1 #replaces REPLACEPATHJPEG
-        subprocess.call(split_version)
-        
-        print(f"{fly_dir} DONE from {date}")
+          # make jpeg path
+          jpeg_path = os.path.join(video_path, 'analysis/', fly_dir)
+          make_dirs(jpeg_path)
+          print(jpeg_path)
+
+          ## run ffmpeg
+
+          final_jpeg_path_v1 = os.path.join(jpeg_path, 'V01frame_%07d.jpg')
+          cmd = f'ffmpeg -i REPLACEPATH REPLACEPATHJPEG -hide_banner -loglevel error'  #have to do it this way because I have spaces in the filepath
+          split_version = cmd.split(" ")
+          split_version[2] = fly_video #replaces REPLACEPATH
+          split_version[3] = final_jpeg_path_v1 #replaces REPLACEPATHJPEG
+          subprocess.call(split_version)
+
+          print(f"{fly_dir} DONE from {date}")
         
         
     
