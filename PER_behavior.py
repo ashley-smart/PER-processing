7fe26d6e4affafaa5bae81ef016577aea170422a
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-07-31 15:46:08 -0700

    making scipy with library

--- PER_behavior.py
+++ PER_behavior.py
@@ -1,3 +1,4 @@
+
 ## rewriting scipy peaks with a library
 
 
@@ -13,14 +14,27 @@ import csv as csv
 import json as json
 import scipy as scipy
 from scipy.signal import find_peaks
+import h5py
 
 
-# get paths
+##### get paths ##########
 date = '20230407'
 fly_id_date_code = 'b-0407'
 
-Path = 'G:/bruker vid 2023/20230407/results/'  #path to results files from read ROIs or DLC
-Savepath = Path
+Path = 'E:/bruker vid 2023/20230407/results/'  #path to results files from read ROIs or DLC
+SavePath = Path + 'h5_files/'
+
+
+####   mostly static variables  ########
+data_reducer = 100
+voltage_framerate = 10000/data_reducer #frames/s # 1frame/.1ms * 1000ms/1s = 10000f/s
+#with reducer i get 1 frame for every .1 * 100 ms => frame/.1*100 ms * 1000ms/s = 100f/s
+#each "frame" is 0.1ms
+video_framerate = 200 #f/s
+#video_framerate_downstairs = 33
+
+
+
 
 #####  import data  ###
 #(allow for bruker light data or video light data)
@@ -31,31 +45,67 @@ Savepath = Path
 # so I don't have to worry about aligning the PER and the light in the same order
 #
 
+files = os.listdir(Path)
+voltage_files = peaks.check_for_voltage_file(Path)
+roi_files = peaks.check_for_results_file(Path)
 
+##look at voltage and roi files and add data to h5 file based on fly#
+if roi_files is not None:
+    for roi_file in roi_files:
+        fly_number = peaks.find_fly_number(roi_file)
+        h5_filename = f'{date}_fly_number_{fly_number}_.h5'
+        h5_Path = os.path.join(SavePath, h5_filename)
+        data = peaks.import_roi_results(os.path.join(Path, roi_file))
+        peaks.add_to_h5(h5_Path, 'roi data', data)
+        if peaks.check_for_key(h5_Path, 'fly-id') == False:
+            fly_id = f'{fly_id_date_code}-{fly_number}'
+            peaks.add_to_h5(h5_Path, 'fly-id', fly_id)
+        
 
+if voltage_files is not None:
+    for voltage_file in voltage_files:
+        fly_number = peaks.find_fly_number(roi_file)
+        h5_filename = f'{date}_fly_number_{fly_number}_.h5'
+        h5_Path = os.path.join(SavePath, h5_filename)
+        data = peaks.import_voltage_data(os.path.join(Path, roi_file))
+        peaks.add_to_h5(h5_Path, 'voltage data', data)
+        if peaks.check_for_key(h5_Path, 'fly-id') == False:
+            fly_id = f'{fly_id_date_code}-{fly_number}'
+            peaks.add_to_h5(h5_Path, 'fly-id', fly_id)
 
 
 #set framerate
-
-
-
-
-
+ # **!!  change so code just asks is voltage framerate or video framerate needed
 
 
 #set interval time
+##since most of my experiments are switch do I need this?
+interval_time = None ##decide if I should change this later
 
-
-
-
-
-
-#set fly numbers
-
-
+#set fly numbers 
+# #(should be set when import data)
 
 
 #get peaks for light and PER
+##look at every flies h5 file and open and run peaks 
+#    (note: some light data may be in roi file)
+#  if there is more than one mean in the data then it will return a list of list of peaks for each mean
+
+h5files = [file for file in os.listdir(SavePath) if '.h5' in file]
+
+for fly in h5files:
+    each_path = os.path.join(SavePath, fly)
+    with h5py.File(each_path, 'a') as f:
+        if ['roi data'] in f.keys():
+            roi_data = f['roi data'][()]
+            data_peaks, properties, columns = peaks.get_peaks(roi_data, each_path)
+        if ['voltage data'] in f.keys():
+            voltage_data = f['voltage data'][()]
+            light_peaks, light_properties, columns = peaks.get_peaks(roi_data, each_path)
+
+
+
+##make plots
 
 
 
--- peaks.py
+++ peaks.py
@@ -9,5 +9,297 @@ import csv as csv
 import json as json
 import scipy as scipy
 from scipy.signal import find_peaks
+import h5py
+
+
+
+## general functions
+def make_empty_h5(savefile, key, dims):
+    """make empty h5 file with specified key and dims as the shape. returns the filename"""
+    with h5py.File(savefile, 'w') as f:
+        dset = f.create_dataset(key, dims, dtype='float32', chunks=True)
+    return savefile
+
+def add_to_h5(Path, key, value):
+    """adds new key value to h5 file and checks if it already exists
+    does overwrite"""
+    with h5py.File(Path, 'a') as f:
+        if key not in f.keys(): #check if key already in file
+            f[key] = value
+        else:
+            del f[key]
+            #print('deleting old key and OVERWRITING')
+            f[key] = value
+
+def check_for_key(h5Path, key):
+    """returns true if key to h5file path exists
+    args:
+        h5Path = path to h5 file
+        key = string want to look for
+    returns:
+        True if found key
+        False if key doesn't exist in file
+        """
+    with h5py.File(h5Path, 'r') as f:
+        if key in f.keys():
+            return True
+        else:
+            return False
+
+
+## set-up functions (import and organize data)
+def check_for_voltage_file (Path):
+    """looks in path for voltage files and returns it/them. otherwise returns None. 
+    if more than one it will return all"""
+    files = os.listdir(Path)
+    all_voltage = []
+    for file in files:
+        if '.csv' in file and 'Voltage' in file:
+            all_voltage.append(file)
+    if len(all_voltage) > 0:
+        return all_voltage
+    else:
+        return None
+    
+def check_for_results_file (Path):
+    """looks in path for Results files and returns it/them. otherwise returns None. 
+    if more than one it will return all"""
+    files = os.listdir(Path)
+    all_results = []
+    for file in files:
+        if '.csv' in file and 'Results' in file:
+            all_results.append(file)
+    if len(all_results) > 0:
+        return all_results
+    else:
+        return None
+    
+
+def import_voltage_data (voltage_path, data_reducer = 100):
+    """takes a single voltage file path and returns a list of the voltage data. 
+    Reduces the data by data reducer amount (every data_reducer(th) element is taken 
+    and the rest is ignored). Default data_reducer number = 100"""
+    if "Voltage" not in voltage_path:
+        print("Caution: this does not contain 'Voltage', make sure it's the right file")
+    light_data = []
+    with open(os.path.join(voltage_path), 'r') as raw_volt:
+        data_single = []
+        volt_reader = csv.reader(raw_volt)
+        for i, row in enumerate(volt_reader):
+            if i % data_reducer == 0:  #this will downsample the data by taking every 100, i % 100 means will divide i by 100 and take the ones that have no remainder
+                data_single.append(row)
+        light_data.append(data_single)
+    return light_data
+
+def import_roi_results (results_path):
+    """takes results filepath and returns a list of all the data in the file"""
+    with open(os.path.join(results_path), 'r') as rawfile:  #'rb' in python2
+        data_single = []
+        reader = csv.reader(rawfile)
+        data_single = list(reader)
+    return data_single
+
+
+def find_fly_number(file):
+    """takes string, looks for fly and then the number following "fly" returns the number"""
+    fly_number = find_number(find_fly_string(file))
+    return fly_number
+
+
+def find_fly_string(string):
+    """things after fly will be returned as string"""
+    post_fly_string = string.split("fly",1)[1]
+    return post_fly_string
+
+def find_number(string):
+    """want this to give number but if split with non-number character to stop. This will give just the first number it finds"""
+    number = ""
+    for i in range(len(string)):
+        current_character = string[i]      
+        if current_character.isdigit():
+            number = number + current_character 
+            if i != len(string)-1: #if it's not the last character
+                next_character = string[i+1]
+                if not next_character.isdigit():
+                    break
+    return number
+
+## probably change this. I don't think I want "asks" anymore
+def get_intervals_and_asks(list_map, file_name_list):
+    """Get the PER interval and the ask type for each file in file_name_list.
+    
+    Args:
+      list_map: A Dict of {interval: [list of filenames]} where interval is either the
+        length of the interval in seconds or one of the strings {'random', 'none'}.
+      file_name_list: A List of string filenames. The order determines the order of the output.
+    Returns:
+      A tuple of lists (interval_ms_list, ask) ordered to match file_name_list.
+      interval_ms_list: a list of the interval length in milliseconds for each result in file_name_list
+      ask: a list of ask types: 0 for fixed intervals, 'r' for random, and 'y' for none.
+    """
+    interval_ms_list = []
+    ask = []
+    for file_name in file_name_list:
+        for key in list_map:
+            if file_name in list_map[key]:
+                if isinstance(key, str): # using strings to pass special values
+                    if key == 'none' or 'y':
+                        interval_ms_list.append(0)
+                        ask.append('y')
+                    if key == 'random':
+                        interval_ms_list.append(0)
+                        ask.append('r')
+                else:  # if the key is not a string, it must be a number.
+                    interval_ms_list.append(key * 1000)
+                    ask.append(0)
+                #print(key, file_name, interval_ms_list[-1], ask[-1]) # optional if you like to print stuff.
+    return interval_ms_list, ask
+
+def generate_fly_number_list(date_code, number_flies):
+    """Return a list of fly names based on date code
+    Args:
+        date code: the string that will form the beginning of the fly name (i.e. 150411)
+        number_flies: the number of flies need date codes for 
+            (can give len(data_all) in current iteration of code)
+    Returns:
+        list of strings with date_code_# (i.e. 150411-2)"""
+    fly_code_list = []
+    for i in range(number_flies):
+        fly_code = str(date_code) + '-' + str(i+1)
+        fly_code_list.append(fly_code)
+    return fly_code_list
+
+
+
+
+#### analysis functions
+def make_bins (time_range, binsize):
+    """to make bins based on desired time range and binsize--using ms
+    args: 
+        time_range = max time in ms to have bins go until
+        binsize = ms per bin
+    returns:
+        array of bins that extend to timerange and are binsize"""
+    ms_binlength = np.asarray(range(int(time_range/binsize) + 1)) * binsize
+    return ms_binlength
+
+def is_column_mean (data, row, column):
+    """return true if cell contains 'mean' for ROI code or has input or diode from voltage data
+    args:
+        data: the data for a fly from csv file
+        row:  row of data to look in
+        column: column of data to look in
+    returns: True if finds keyword"""
+    
+    return "Mean" in data[row][column] or "diode" in data[row][column] or " Input 0" in data[row][column] 
+
+def get_header(data, column_number):  #this will pull from whatever you call the variable that you have gotten your csv data from and specified column 
+    title_list = []
+    """gets header (row 0) for data set for specified column (data, column number)"""
+    for row in data: #row is the variable with the row number in it if use range(len(data)) it gives a list of the row if just use data
+        #print row
+        title_list.append(row[column_number])
+    titles = np.array(title_list[:1]) #changes the list into a numpy array. [:1] means take the first row
+    return titles
+
+def get_mean_titles(data):
+    """data is raw data for one fly, returns the titles of every column that has Mean in it"""
+    #1. get mean column indices
+    mean_indices = get_mean_indices(data)
+
+    #2. get array with titles
+    title_collection = []
+    for t in mean_indices:
+        title_collection.append(get_header(data, t))
+    if len(title_collection) > 1:
+        mean_titles = np.vstack(title_collection).T
+    else:
+        mean_titles = title_collection
+    return mean_titles
+
+def get_mean_indices(data):
+	"""get indices where there are means from data from one fly (previous code this was called mean_column"""
+	mean_indices = []
+	for row in range(len(data)):
+		for column in range(len(data[row])):
+			if is_column_mean(data, row, column):
+				mean_indices.append(column) #store column index
+	return mean_indices
+
+def get_means(data, column_number):  #this will pull from whatever you call the variable that you have gotten your csv data from and specified column 
+    mean_list = []
+    """gets column for (data, column_number) and eliminates the header (row 0) and puts it into a np.array"""
+    for row in data: #row is the variable with the row number in it if use range(len(data)) it gives a list of the row if just use data
+        #print row
+        mean_list.append(row[column_number])
+    means = np.asarray(mean_list[1:], dtype=float) #changes the list into a numpy array. [1:] means take the second number to the end 
+    #(to get rid of the title). dtype changes from strings to np integers
+    return means
+
+def is_column_in_list(data, row, column, str_list):
+	return data[row][column] in str_list
+
+def is_column_light(data, row, column):
+	return is_column_in_list(data, row, column, ("Light", "light", "diode", "Mean(Light)", " Input 0"))
+
+
+
+
+##
+def get_peaks(data, h5file,  override = False,):
+    """ uses scipy peaks to find peak points of data 
+    finds negative peaks for PER and positive peaks for light. 
+    Will also add the peaks to the h5 file of the fly
+    args: 
+        data = raw data imported that contains a header with "mean", "diode", "input 0"
+        h5file = path to h5 file data to save peaks data
+        override = False as default. If true then it allows the 
+            function to find peaks for data that has no header 
+            (will assume it is PER and do negative peaks)
+            data must then be a single array
+    returns:
+        peaks = the indices where there are peaks
+        peak_properties = dict of various properties from scipy peaks
+        columns = raw data"""
+    
+    ##look through data and find if there is light or PERs
+    #1. get mean column indices and mean titles
+    mean_indices = get_mean_indices(data)
+    mean_titles = get_mean_titles(data)
+
+    if len(mean_indices) < 1:
+        if override == True:
+            squeeze_column = np.squeeze(data)
+            peaks, properties = scipy.signal.find_peaks(squeeze_column*-1, prominence = 3, distance = 15) 
+            return peaks, properties, squeeze_column
+        else:
+            raise Exception(f'There is no identified mean index--If due to lack of header consider overriding and inputting single array')
+        
+    #2. find peaks
+    all_peaks= []
+    columns = [] #needed for boolean onsets later
+    for mean_index in range(len(mean_indices)):
+        single_column = get_means(data, mean_indices[mean_index])
+        #to get each element out of their own array and into one array with all elements
+        squeeze_column = np.squeeze(single_column) 
+        if is_column_light(mean_titles, 0, mean_index) or 'diode' in mean_titles[0][0]:
+            print('light')
+            #peaks, _ = scipy.signal.find_peaks(squeeze_column, prominence = .5)
+            columns.append(single_column)
+            ##modification for noisy diode data
+            light_median = np.median(single_column, axis = 0)
+            early_light_max = max(single_column[0:2000])
+            #peaks, _ = scipy.signal.find_peaks(squeeze_column, height = light_median[0], prominence = .3, distance = 10)
+            peaks, properties = scipy.signal.find_peaks(squeeze_column, height = early_light_max +.001, prominence = .1, distance = 10)
+            add_to_h5(h5file, 'light peaks', peaks)
+            
+        else:
+            print('PER')
+            peaks, properties = scipy.signal.find_peaks(squeeze_column*-1, prominence = 3, distance = 15) 
+            columns.append(single_column)
+            add_to_h5(h5file, 'PER peaks', peaks)
+            #distance is req frames between peaks
+        all_peaks.append(peaks)
+    return all_peaks, properties, columns
 
 
